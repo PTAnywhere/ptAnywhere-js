4c0524b968ce1602f2436043cf53dfc8f7bd5426
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-11-30 10:50:15 +0000

    Creating hierarchical suites and adding setUp and tearDown methods.

--- test/libPTAnywhereTest.js
+++ test/libPTAnywhereTest.js
@@ -1,43 +1,89 @@
-
 describe("packetTracer module", function() {
 
+  var apiURL = 'http://192.168.34.202:8080/api/v1';
+  var fileToOpen = 'http://192.168.34.202:8080/files/ibookdemo611.pkt';
+
   it("creates a new session", function(done) {
-    var apiURL = 'http://192.168.34.202:8080/api/v1';
-    var fileToOpen = 'http://192.168.34.202:8080/files/ibookdemo611.pkt';
+
     packetTracer
       .newSession(apiURL, fileToOpen, function(newSessionURL) {
         // For some reason, when called using Jasmine
         // xhr.getAllResponseHeaders() does not return the 'Location' header
-        //expect(newSessionURL).not.toBe(null);
-        done();
+        expect(newSessionURL).not.toBe(null);
+        packetTracer.destroySession(newSessionURL, function() {
+          done();
+        });
       })
-      .fail( function() {
+      .fail(function() {
         done.fail("Session creation has failed.");
       });
   });
 
-  it("creates device after deleting", function(done) {
-    var apiURL = 'http://192.168.34.202:8080/api/v1/sessions/xclzDg85Qvi_4VHL0sBmkA--';
-    var cli = new packetTracer
-                    .Client(apiURL, function() {
-                      fail("The session has expired.");
-                    });
-    cli.getNetwork(function(network) {
-      var d = network.devices[0];
-      cli.removeDevice(d).done(function() {
-        cli.addDevice(d, function() {
-          done();
-        }).fail(function(jqXHR, textStatus, errorThrown) {
-          done.fail("The device was not added.");
+  describe("Client object", function() {
+    var sessionUrl;
+    var client;
+
+    beforeEach(function(done) {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+      packetTracer.newSession(apiURL, fileToOpen, function(newSessionUrl) {
+        sessionUrl = newSessionUrl;
+        console.log("Creating client for session URL " + sessionUrl);
+        client = new packetTracer.Client(sessionUrl, function() {
+          done.fail("The session has expired.");
         });
-      }).fail(function() {
-        done.fail("The device was not removed.");
+        done();
       });
-    },
-    function(tryCount, maxRetries, errorCode) {
-    }).fail(function() {
-      done.fail("The network was not loaded.");
     });
+
+    afterEach(function(done) {
+      client = null;
+      packetTracer.destroySession(sessionUrl, function() {
+        done();
+      });
+    });
+
+    // Related issue: https://github.com/PTAnywhere/ptAnywhere-js/issues/1
+    it("creates device after deleting device", function(done) {
+      client.getNetwork(function(network) {
+        var d = network.devices[0];
+        client.removeDevice(d).done(function() {
+          client.addDevice({group: 'pc', x: 0, y: 0}, function() {
+            done();
+          }).fail(function(jqXHR, textStatus, errorThrown) {
+            done.fail("The device was not added.");
+          });
+        }).fail(function() {
+          done.fail("The device was not removed.");
+        });
+      },
+      function(tryCount, maxRetries, errorCode) {
+        done.fail("Timeout getting the network.");
+      }).fail(function() {
+        done.fail("The network was not loaded.");
+      });
+    });
+
+    // Related issue: https://github.com/PTAnywhere/ptAnywhere-js/issues/1
+    it("creates device after deleting link", function(done) {
+      client.getNetwork(function(network) {
+        var l = network.edges[0];
+        client.removeLink(l).done(function() {
+          client.addDevice({group: 'pc', x: 0, y: 0}, function() {
+            done();
+          }).fail(function(jqXHR, textStatus, errorThrown) {
+            done.fail("The device was not added.");
+          });
+        }).fail(function() {
+          done.fail("The link was not removed.");
+        });
+      },
+      function(tryCount, maxRetries, errorCode) {
+        done.fail("Timeout getting the network.");
+      }).fail(function() {
+        done.fail("The network was not loaded.");
+      });
+    });
+
   });
 
 });
